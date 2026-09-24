6b27c09cd232a10cc1060ab1e4d494009e7b41ef
zahraasalih0 <zahraasalih708@gmail.com>
2026-09-24 13:40:13 +0300

    جاهز للنشر

--- app.py
+++ app.py
@@ -43,4 +43,5 @@ def particles_asset():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
